4773b7f7a7dfd18ac6fdbe2ab84b14bf21a6eccd
Владимир <vovanpadawan@gmail.com>
2018-05-03 10:03:08 +0300

    Домашка по уроку 3. Надо доделать компонент и понять почему письма не ходят

---
+++ Lesson3Homework/MailSender/EmailSendServiceLib/EmailSendService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Collections.ObjectModel;
+
+namespace EmailSendServiceLib
+{
+    public sealed class EmailSendService : IDisposable
+    {
+        SmtpClient _client;
+
+        /// <summary>
+        /// Конструктор класса, принимает параметры для создания почтового клиента, 
+        /// а так же данные по отправителю письма, могут быть нулл
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="ssl"></param>
+        /// <param name="credential"></param>
+        public EmailSendService(string host, int port, bool ssl, NetworkCredential credential)
+        {
+            _client = new SmtpClient(host, port) { EnableSsl = ssl, Credentials = credential };
+        }
+
+        private EmailSendService() { }
+
+
+        private void SendMailAsync(MailMessage message)
+        {
+            _client.SendAsync(message, null);
+        }
+
+        public void SendMailToAll(string from, string subject, string body, List<string> addresses)
+        {
+            foreach(string address in addresses)
+            {
+                MailMessage message = new MailMessage(from, address, subject, body);
+            }
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+    }
+}
---
+++ Lesson3Homework/MailSender/MailSenderControlsLib/TabSvitcher.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSenderControlsLib
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class TabSvitcher
+    {
+        public event RoutedEventHandler Back;
+        public event RoutedEventHandler Forvard;
+
+        public TabSvitcher() => InitializeComponent();
+
+        private void BackvardButton_Click(object sender, RoutedEventArgs e) => Back?.Invoke(this, new RoutedEventArgs());
+        private void ForvardButton_Click(object sender, RoutedEventArgs e) => Forvard?.Invoke(this, new RoutedEventArgs());
+    }
+}
