cb3c12e548a94921950960bb41cbe1baa1202bd1
Владимир <vovanpadawan@gmail.com>
2018-05-06 11:20:58 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Lesson4Homework/MailSender/EmailSendServiceLibTest/EmailSendServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmailSendServiceLibTest
+{
+    [TestClass]
+    public class EmailSendServiceTest
+    {
+        [TestMethod]
+        public void SendMailToAll_Test()
+        {
+
+        }
+    }
+}
---
+++ Lesson4Homework/MailSender/EmailSendServiceLibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmailSendServiceLibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmailSendServiceLibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf4c3384-16c6-4fc4-aaec-f652badac4ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
